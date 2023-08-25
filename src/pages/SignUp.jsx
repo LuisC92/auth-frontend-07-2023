8a27797220f4b1d44a4dc2974f6960c5764bcb72
LuisC92 <luis.clara@wildcodeschool.com>
2023-08-25 11:21:05 +0100

    first commit

---
+++ src/pages/SignUp.jsx
@@ -0,0 +1,54 @@
+import api from "../api/api";
+import { useForm } from "react-hook-form";
+
+const SignUp = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const createUser = (data) => {
+    // console.log(data);
+    api
+      .post("/auth/sign-up", data)
+      .then((response) => {
+        console.log(response);
+      })
+      .catch((error) => {
+        if (error.response.status === 401) {
+          console.error(error);
+          alert(error.response.data);
+        }
+      });
+  };
+
+  return (
+    <div>
+      <h1>Sign-Up</h1>
+      <form onSubmit={handleSubmit(createUser)}>
+        <label>email:</label>
+        <br />
+        <input
+          type="email"
+          {...register("email", { required: "email is required" })}
+          aria-invalid={errors.email ? "true" : "false"}
+        />
+        {errors.email && <p>{errors.email?.message}</p>}
+        <br />
+        <label>password:</label>
+        <br />
+        <input
+          type="password"
+          {...register("password", { required: "password is required" })}
+          aria-invalid={errors.password ? "true" : "false"}
+        />
+        {errors.password && <p>{errors.password?.message}</p>}
+        <br />
+        <button type="submit">Register</button>
+      </form>
+    </div>
+  );
+};
+
+export default SignUp;
